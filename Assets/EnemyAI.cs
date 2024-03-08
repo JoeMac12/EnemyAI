b4187cd0d65d0ec14af0e061b94ee617970ccea5
Arman <144450534+JoeMac12@users.noreply.github.com>
2024-03-08 14:03:27 -0400

    start setting up enemy script

--- Assets/EnemyAI.cs
+++ Assets/EnemyAI.cs
@@ -6,4 +6,21 @@ using TMPro;
 
 public class EnemyAI : MonoBehaviour
 {
+	public NavMeshAgent agent;
+	public Transform player;
+	public Transform[] patrolPoints;
+	public AIState currentState;
+	private int patrolPointIndex = 0;
+
+	public float sightRange = 10f;
+	public float attackRange = 2f;
+	public float loseSightRange = 15f;
+	public float searchDuration = 5f;
+	public Vector3 lastKnownPosition;
+	private float searchTimer;
+
+	public TextMeshProUGUI stateText;
+	private float distanceToPlayer;
+
+	public enum AIState { Patrolling, Chasing, Searching, Attacking, Retreating } // States
 }
